fbfc4fd6f23df5d4651d3ca64e0ab4b51743cb12
kirigaya1456 <88945011+kirigaya1456@users.noreply.github.com>
2025-09-19 05:45:56 +0000

    commit

---
+++ Lab_1/Work/sandbox/Manukyan.asm
@@ -0,0 +1,15 @@
+format ELF64
+public _start
+msg db "Hello, world", 0xA, 0
+
+_start:
+    ;инициализация регистров для вывода информации на экран
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, msg
+    mov rdx, 14
+    int 0x80
+    ;инициализация регистров для успешного завершения работы программы
+    mov rax, 1
+    mov rbx, 0
+    int 0x80
